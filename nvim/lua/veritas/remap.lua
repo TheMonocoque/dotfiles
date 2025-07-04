aaec1c7423c7b9672944e47660bc2f3c56adf2f3
TheMonocoque <7273463+TheMonocoque@users.noreply.github.com>
2025-07-04 07:50:22 -0400

    Add ruff check to nvim

--- nvim/lua/veritas/remap.lua
+++ nvim/lua/veritas/remap.lua
@@ -11,5 +11,6 @@ vim.keymap.set("n", "<F9>", ':!%:p<CR>')
 
 -- callout to formatting tools
 vim.keymap.set("n", "<leader>fmb", "<CMD>!black -q -l 130 %<CR>")
+vim.keymap.set("n", "<leader>fmr", "<CMD>!ruff check --fix %<CR>")
 vim.keymap.set("n", "<leader>fmj", "<CMD>!jsonnetfmt -i -n 2 %<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
